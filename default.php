18ab1c8076bf1ada69f0eb459b39b14985619f6d
Alexander Tuttle <amt262@uakron.edu>
2023-02-14 21:40:56 -0500

    Create homepage template

--- default.php
+++ default.php
@@ -1,6 +1,37 @@
+<!DOCTYPE html>
 <html>
-  <title>Home Team Creativity</title>
+  <head>
+    <title>Home Team Creativity</title>
+    <script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
+    <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
   <body>
-  <h1>Hello, just creating my own branch for now!</h1>
-  <body>
+    <div class="navbar">
+      <div class="logo">
+      <a href="#" style="padding:0px;"><img src="logo.png" alt="Home Team Creativity Logo" style="width: 23%; height: 23%; hover: white;"></a>
+      </div>
+      <a href="#">Products</a>
+      <a href="#">How It Works</a>
+      <a href="#">About Us</a>
+    </div>
+
+    <div class="row">
+      <div class="side">
+        <h2>Order Now Button Here</h2>
+      </div>
+      <div class="main">
+        <h2>Recent Products Here</h2>
+      </div>
+    </div>
+
+    <div class="footer">
+      <h2>Thank you for supporting small businesses!</h2>
+    </div>
+
+
+  </body>
 </html>
