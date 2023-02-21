f95766eb1fce0e3cec76fb3daeecede5be9bf025
Alexander Tuttle <amt262@uakron.edu>
2023-02-21 18:33:49 -0500

    Add connectivity to MySql through Wamp

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,53 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Login from "./login"
 import logo from "./assets/logo.png"
 
 function Index () {
-    return (
-      <div className="index">
-        <div className="row">
-          <div className="side">
-            <h2>Order Now Button Here</h2>
-          </div>
-          <div className="main">
-            <h2>Featured Products Here</h2>
-          </div>
-    </div>
+  return (
+    <div className="index">
+      <div className="row">
+        <div className="side">
+          <h2>Order Now Button Here</h2>
+        </div>
+        <div className="main">
+          <h2>Featured Products Here</h2>
+        </div>
       </div>
-    );
+    </div>
+  );
 }
 
 function Homepage() {
-    const [isLoginPage, setIsLoginPage] = useState(false);
+  const [users, setUsers] = useState([]);
 
-    const handleLoginButton = () => {
-      setIsLoginPage(true);
-    }
+  useEffect(() => {
+    fetch('http://localhost:80/PHP/server.php')
+      .then(response => response.json())
+      .then(data => setUsers(data));
+  }, []);
 
-    const handleHomepageButton = () => {
-      setIsLoginPage(false);
-    }
+  const [isLoginPage, setIsLoginPage] = useState(false);
 
-    return (
-      <div className="HomePage">
-        <div className="App">
-          <div className="navbar">
-          <button onClick={handleHomepageButton} className="homepageButton">
-            <img src={logo} alt="Home Team Creativity Logo" className="logo"/>
-            </button>
-            <a href="#">Products</a>
-            <a href="#">How It Works</a>
-            <a href="#">About Us</a>
-            <button onClick={handleLoginButton} className="loginButton">Login/Sign Up</button>
-          </div>
+  const handleLoginButton = () => {
+    setIsLoginPage(true);
+  }
+
+  const handleHomepageButton = () => {
+    setIsLoginPage(false);
+  }
+
+  return (
+    <div className="HomePage">
+      <div className="App">
+        <div className="navbar">
+        <button onClick={handleHomepageButton} className="homepageButton">
+          <img src={logo} alt="Home Team Creativity Logo" className="logo"/>
+          </button>
+          <a href="#">Products</a>
+          <a href="#">How It Works</a>
+          <a href="#">About Us</a>
+          <button onClick={handleLoginButton} className="loginButton">Login/Sign Up</button>
         </div>
-        { isLoginPage ? (
-          <Login />
-        ) : (
-          <Index />
-        ) }
       </div>
-    );
+      { isLoginPage ? (
+        <Login />
+      ) : (
+        <Index />
+      ) }
+      <h1>Users</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.email}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Homepage;
