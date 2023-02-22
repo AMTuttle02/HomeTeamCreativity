074472cce50b1a23bbd4d2fc0be90c0439ccfc07
Alexander Tuttle <amt262@uakron.edu>
2023-02-22 11:21:21 -0500

    Add products page file

---
+++ src/products.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./index.css";
+
+function Login() {
+
+  return (
+    <div className="Products">
+      <h1>Hello, Welcome to the Products Page!</h1>
+    </div>
+  );
+}
+export default Login;
