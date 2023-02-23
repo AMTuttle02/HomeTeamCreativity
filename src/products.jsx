976d609a47d3a5a83b88cfabedb68275db8d5f21
Alexander Tuttle <amt262@uakron.edu>
2023-02-23 16:44:05 -0500

    Add sample products to product page

--- src/products.jsx
+++ src/products.jsx
@@ -1,12 +1,49 @@
 import React, { useState } from "react";
 import "./index.css";
+import tshirt from "./assets/blackTShirt.png";
+import design from "./assets/designOne.png";
 
-function Login() {
+function Products() {
 
   return (
     <div className="Products">
-      <h1>Hello, Welcome to the Products Page!</h1>
+      <h1>Products</h1>
+      <p>For custom apparel, send us a message</p>
+      <div className="productsTable">
+        <div className="productsTr">
+          <div className="productsTd">
+            <div className="fullDesign">
+              <img src={tshirt} alt="Home Team Creativity Logo" className="tshirt"/>
+              <img src={design} alt="Home Team Creativity Logo" className="design"/>
+              <p>Product Title</p>
+              <br />
+              <p>Product Price</p>
+            </div>
+            <div className="fullDesign">
+              <img src={tshirt} alt="Home Team Creativity Logo" className="tshirt"/>
+              <img src={design} alt="Home Team Creativity Logo" className="design"/>
+              <p>Product Title</p>
+              <br />
+              <p>Product Price</p>
+            </div>
+            <div className="fullDesign">
+              <img src={tshirt} alt="Home Team Creativity Logo" className="tshirt"/>
+              <img src={design} alt="Home Team Creativity Logo" className="design"/>
+              <p>Product Title</p>
+              <br />
+              <p>Product Price</p>
+            </div>
+            <div className="fullDesign">
+              <img src={tshirt} alt="Home Team Creativity Logo" className="tshirt"/>
+              <img src={design} alt="Home Team Creativity Logo" className="design"/>
+              <p>Product Title</p>
+              <br />
+              <p>Product Price</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-export default Login;
+export default Products;
