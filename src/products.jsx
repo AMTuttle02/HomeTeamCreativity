0fc66e0988f26ae36e5d4b9f04938b8a19b291c1
Alexander Tuttle <amt262@uakron.edu>
2023-02-24 18:08:25 -0500

    Implement multiple designs

--- src/products.jsx
+++ src/products.jsx
@@ -19,36 +19,15 @@ function Products() {
       <div className="productsTable">
         <div className="productsTr">
           <div className="productsTd">
-            <div className="fullDesign">
+            {products.map(product => (
+              <div className="fullDesign">
               <img src={tshirt} alt="Home Team Creativity Logo" className="tshirt"/>
-              {products.map(product => (
-                <img src={"src/assets/" + product.filename} alt={product.filename} className="design"/>
-              ))}
-              <p>Product Title</p>
+              <img src={"src/assets/" + product.filename} alt={product.filename} className="design"/>
+              <p>{product.title}</p>
               <br />
-              <p>Product Price</p>
-            </div>
-            <div className="fullDesign">
-              <img src={tshirt} alt="Home Team Creativity Logo" className="tshirt"/>
-              <img src={design} alt="Home Team Creativity Logo" className="design"/>
-              <p>Product Title</p>
-              <br />
-              <p>Product Price</p>
-            </div>
-            <div className="fullDesign">
-              <img src={tshirt} alt="Home Team Creativity Logo" className="tshirt"/>
-              <img src={design} alt="Home Team Creativity Logo" className="design"/>
-              <p>Product Title</p>
-              <br />
-              <p>Product Price</p>
-            </div>
-            <div className="fullDesign">
-              <img src={tshirt} alt="Home Team Creativity Logo" className="tshirt"/>
-              <img src={design} alt="Home Team Creativity Logo" className="design"/>
-              <p>Product Title</p>
-              <br />
-              <p>Product Price</p>
-            </div>
+              <p>{"$" + product.price}</p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
