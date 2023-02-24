6b62fd6d52c3da249ec764e9b208f74321d8df0a
Alexander Tuttle <amt262@uakron.edu>
2023-02-24 17:43:27 -0500

    Add first design to shirt

--- src/products.jsx
+++ src/products.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import "./index.css";
 import tshirt from "./assets/blackTShirt.png";
 import design from "./assets/designOne.png";
 
 function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:80/PHP/products.php')
+      .then(response => response.json())
+      .then(data => setProducts(data));
+  }, []);
 
   return (
     <div className="Products">
@@ -14,7 +21,9 @@ function Products() {
           <div className="productsTd">
             <div className="fullDesign">
               <img src={tshirt} alt="Home Team Creativity Logo" className="tshirt"/>
-              <img src={design} alt="Home Team Creativity Logo" className="design"/>
+              {products.map(product => (
+                <img src={"src/assets/" + product.filename} alt={product.filename} className="design"/>
+              ))}
               <p>Product Title</p>
               <br />
               <p>Product Price</p>
