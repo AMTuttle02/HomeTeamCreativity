f48afa21db749ddb309e71a43d09c6b38a5e95c5
Alexander Tuttle <amt262@uakron.edu>
2023-03-17 13:40:40 -0400

    Add HomeContents file

---
+++ src/HomeContents.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function HomeContents() {
+    return (
+        <div className="index">
+          <div className="row">
+            <div className="side">
+              <h2>Order Now Button Here</h2>
+            </div>
+            <div className="main">
+              <h2>Featured Products Here</h2>
+            </div>
+          </div>
+        </div>
+      );
+}
+
+export default HomeContents;
