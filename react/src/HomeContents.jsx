755b21b6646cd708bcdd6da7de8f2f04586f7560
Alexander Tuttle <amt262@uakron.edu>
2023-03-22 18:19:31 -0400

    Add content to the homepage

--- react/src/HomeContents.jsx
+++ react/src/HomeContents.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
+import tshirt from "./assets/blackTShirt.png";
+import { Outlet, Link } from "react-router-dom";
 
 function HomeContents() {
   const [firstName, setFirstName] = useState("");
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     fetch("/api/session.php")
@@ -9,6 +12,9 @@ function HomeContents() {
       .then((data) => {
         setFirstName(data.first_name);
       });
+    fetch("/api/featuredProducts.php")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
   }, []);
 
   return (
@@ -16,12 +22,42 @@ function HomeContents() {
       {firstName ? <h1>Welcome Back {firstName}!</h1> : <h1><b>Welcome to Home Team Creativity!</b></h1>}
       <div className="row">
         <div className="side">
-          <h2>Order Now Button Here</h2>
+          <div className="orderLinks">
+            <br /><br /><br />
+            <Link to='/products' className="OrderButton">ORDER NOW</Link>
+            <br /><br /><br /><br /><br />
+            <Link to='/about' className="OrderButton">ABOUT US</Link>
+            <br /><br /><br /><br /><br />
+            <Link to='mailto:hometeamcreativity@gmail.com' className="OrderButton">CONTACT US</Link>
+          </div>
         </div>
         <div className="main">
-          <h2>Featured Products Here</h2>
+          <h2>Featured Products</h2>
+          <div className="productsTable">
+            <div className="productsTdHomepage">
+              {products.map((product) => (
+                <div key={product.filename}>
+                  <div className="fullDesign">
+                    <img
+                      src={tshirt}
+                      alt="Home Team Creativity Logo"
+                      className="tshirt"
+                    />
+                    <img
+                      src={"src/assets/" + product.filename}
+                      alt={product.filename}
+                      className="design"
+                    />
+                    <p>{product.product_name}</p>
+                    <p>{"$" + product.price}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
+      <Outlet />
     </div>
   );
 }
