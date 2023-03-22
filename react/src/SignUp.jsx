a2917b0240233df08f9fe81e075d1608f1003cba
Alexander Tuttle <amt262@uakron.edu>
2023-03-22 16:14:36 -0400

    Finish signup functionality

--- react/src/SignUp.jsx
+++ react/src/SignUp.jsx
@@ -1,6 +1,14 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Outlet, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+function SignUpFailed() {
+  return (
+    <div className="incorrectPassword">
+      <h2>Sorry, that email is already registered.</h2>
+    </div>
+  );
+}
 
 function SignUp() {
   const [password, setPassword] = useState("");
@@ -11,6 +19,8 @@ function SignUp() {
   const [emailError, setemailError] = useState("");
   const [fnameError, setfnameError] = useState("");
   const [lnameError, setlnameError] = useState("");
+
+  const [badLogin, setBadLogin] = useState("");
 
   const handleValidation = (event) => {
     let formIsValid = true;
@@ -62,92 +72,120 @@ function SignUp() {
 
   const signUpSubmit = (e) => {
     e.preventDefault();
-    handleValidation();
+    if (handleValidation()) {
+      fetch('/api/signup.php', {  
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ 
+          "fname":fname, 
+          "lname" :lname,
+          "email" : email,
+          "password" : password
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // If the email and password are valid, redirect to the homepage
+          if (data.loggedin) {
+            window.location.href = '/loggedin';
+          } else {
+            // If the email and password are not valid, display an error message
+            setBadLogin(true);
+            console.log(data);
+          }
+        });
+    }
   };
 
-  return (
-    <div className="SignUp">
-      <br/>
-      <div className="container">
-        <h1><u>Sign Up Below!</u></h1>
-        <form id="signupform" onSubmit={signUpSubmit}>
-          <label>First Name</label>
-          <input
-            type="name"
-            className="form-control"
-            id="NameInput"
-            name="NameInput"
-            aria-describedby="nameHelp"
-            placeholder="Enter your first name"
-            onChange={(event) => setFName(event.target.value)}
-          />
-          <small id="nameHelp" className="text-danger form-text">
-            {fnameError}
-          </small>
-          <br/>
-          <label>Last Name</label>
-          <input
-            type="name"
-            className="form-control"
-            id="NameInput"
-            name="NameInput"
-            aria-describedby="nameHelp"
-            placeholder="Enter your last name"
-            onChange={(event) => setLName(event.target.value)}
-          />
-          <small id="nameHelp" className="text-danger form-text">
-            {lnameError}
-          </small>
-          <br/>
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            id="EmailInput"
-            name="EmailInput"
-            aria-describedby="emailHelp"
-            placeholder="Enter email"
-            onChange={(event) => setEmail(event.target.value)}
-          />
-          <small id="emailHelp" className="text-danger form-text">
-            {emailError}
-          </small>
-          <br/>
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-            placeholder="Password"
-            onChange={(event) => setPassword(event.target.value)}
-          />
-          <small id="passworderror" className="text-danger form-text">
-            {passwordError}
-          </small>
-          <br/>
-          <button type="submit">
-            Sign In
-          </button>
-        </form>
+  const [firstName, setFirstName] = useState("");
+
+  useEffect(() => {
+    fetch("/api/session.php")
+      .then((response) => response.json())
+      .then((data) => {
+        setFirstName(data.first_name);
+      });
+  }, []);
+
+  if (firstName) {
+    window.location.href='/loggedin';
+  }
+  else {
+    return (
+      <div className="SignUp">
+        <br/>
+        <div className="container">
+          <h1><u>Sign Up Below!</u></h1>
+          <form id="signupform" onSubmit={signUpSubmit}>
+            <label>First Name</label>
+            <input
+              type="name"
+              className="form-control"
+              id="NameInput"
+              name="NameInput"
+              aria-describedby="nameHelp"
+              placeholder="Enter your first name"
+              onChange={(event) => setFName(event.target.value)}
+            />
+            <small id="nameHelp" className="text-danger form-text">
+              {fnameError}
+            </small>
+            <br/>
+            <label>Last Name</label>
+            <input
+              type="name"
+              className="form-control"
+              id="NameInput"
+              name="NameInput"
+              aria-describedby="nameHelp"
+              placeholder="Enter your last name"
+              onChange={(event) => setLName(event.target.value)}
+            />
+            <small id="nameHelp" className="text-danger form-text">
+              {lnameError}
+            </small>
+            <br/>
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="EmailInput"
+              name="EmailInput"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <small id="emailHelp" className="text-danger form-text">
+              {emailError}
+            </small>
+            <br/>
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            <small id="passworderror" className="text-danger form-text">
+              {passwordError}
+            </small>
+            <br/>
+            { badLogin && <SignUpFailed /> }
+            <button type="submit">
+              Sign Up
+            </button>
+          </form>
+        </div>
+        <div className="UserAccess">
+          <br/><br/>
+          <p>Already Have An Account?
+          <Link to="/login" className="signUpButton">Login </Link>
+          </p>
+        </div>
+        <Outlet/>
       </div>
-      <div className="UserAccess">
-        <br/><br/>
-        <p>Already Have An Account?
-        <Link to="/login" className="signUpButton">Login </Link>
-        </p>
-      </div>
-      <Outlet/>
-    </div>
-  );
+    );
+  }
 }
 export default SignUp;
-/*
-fetch('signup.php', {  
-  method: "POST",
-  body: JSON.stringify({ 
-    "first name":fname, 
-    "last name" :lname,
-    "email" : email,
-    "password" : password
-  }),
-})*/
