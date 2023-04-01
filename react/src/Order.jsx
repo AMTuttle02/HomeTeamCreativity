390f1070dd94c51a46f529c18e3790b9be6807f1
Alexander Tuttle <amt262@uakron.edu>
2023-04-01 18:12:52 -0400

    Add sizing and color options

--- react/src/Order.jsx
+++ react/src/Order.jsx
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
 import tshirt from "./assets/blackTShirt.png";
+import transparentTshirt from "./assets/transparentTshirt.png";
+import transparentLongSleeve from "./assets/transparentLongSleeve.png";
+import transparentCrewneck from "./assets/transparentCrewneck.png";
+import transparentHoodie from "./assets/transparentHoodie.png";
+import black from "./assets/black.png";
+import red from "./assets/red.png";
+import yellow from "./assets/yellow.png";
+import blue from "./assets/blue.png";
 
 function Order() {
   return (
@@ -26,15 +34,70 @@ function Order() {
         <div className="orderMain">
           <h3>Price calculated after selections</h3>
           <h1>Style: Short Sleeve T-Shirt</h1>
-          <h1>Add Images Here</h1>
-          <h1>And Here</h1>
-          <h1>And Here</h1>
+          <div class="typeOptionRow">
+            <img
+                src={transparentTshirt}
+                alt="Home Team Creativity Logo"
+                className="shirtOptions"
+            />
+            <img
+                src={transparentLongSleeve}
+                alt="Home Team Creativity Logo"
+                className="shirtOptions"
+            />
+            <img
+                src={transparentCrewneck}
+                alt="Home Team Creativity Logo"
+                className="shirtOptions"
+            />
+            <img
+                src={transparentHoodie}
+                alt="Home Team Creativity Logo"
+                className="shirtOptions"
+            />
+          </div>
+          <br />
           <h1>Color: Black</h1>
-          <h1>Add color options here</h1>
+          <div class="typeOptionRow">
+            <img
+                src={black}
+                alt="Home Team Creativity Logo"
+                className="colorOptions"
+            />
+            <img
+                src={red}
+                alt="Home Team Creativity Logo"
+                className="colorOptions"
+            />
+            <img
+                src={yellow}
+                alt="Home Team Creativity Logo"
+                className="colorOptions"
+            />
+            <img
+                src={blue}
+                alt="Home Team Creativity Logo"
+                className="colorOptions"
+            />
+          </div>
           <br />
           <h1>Size: Adult Medium</h1>
-          <h1>Add Youth Size Options here</h1>
-          <h1>Add Adult Size Options here</h1>
+          <div class="typeOptionRow">
+            <p className="size">YOUTH:</p>
+            <p className="size">Small</p>
+            <p className="size">Medium</p>
+            <p className="size">Large</p>
+            <p className="size">X-Large</p>
+          </div>
+          <div class="typeOptionRow">
+            <p className="size">ADULT:</p>
+            <p className="size">Small</p>
+            <p className="size">Medium</p>
+            <p className="size">Large</p>
+            <p className="size">X-Large</p>
+            <p className="size">XX-Large</p>
+          </div>
+          <br />
           <h1>Quantity: 1</h1>
           <center>
           <button>Add to Cart</button>
