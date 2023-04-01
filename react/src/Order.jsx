12cc36766ad6745c796a43b465ffd1cb608df7d5
Alexander Tuttle <amt262@uakron.edu>
2023-04-01 12:58:07 -0400

    Add order page

---
+++ react/src/Order.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import tshirt from "./assets/blackTShirt.png";
+
+function Order() {
+  return (
+    <div className="Order">
+        <h1>Hi</h1>
+    </div>
+  );
+}
+export default Order;
