cacddeab5aaa7e3394b0412e6a26afe03052e2a1
Alexander Tuttle <amt262@uakron.edu>
2023-04-01 13:27:38 -0400

    Add product preview to order page

--- react/src/Order.jsx
+++ react/src/Order.jsx
@@ -4,7 +4,29 @@ import tshirt from "./assets/blackTShirt.png";
 function Order() {
   return (
     <div className="Order">
-        <h1>Hi</h1>
+      <div className="row">
+        <div className="orderSide">
+          <div className="orderFullDesign">
+            <img
+            src={tshirt}
+            alt="Home Team Creativity Logo"
+            className="orderTshirt"
+            />
+            <br /><br />
+            <h3>Product Name</h3>
+            <br />
+            <p>Details:</p>
+            <p>100% Cotton</p>
+            <p>True To Size</p>
+            <p>Regualr Fit</p>
+            <p>Wash Inside Out if possible</p>
+            <a href=''>Return Policy</a>
+          </div>
+        </div>
+        <div className="main">
+            <h1>Hi</h1>
+        </div>
+      </div>
     </div>
   );
 }
