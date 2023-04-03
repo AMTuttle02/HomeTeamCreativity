e825b353f48ef43e4f30ef15f2978cb50af6cf36
Alexander Tuttle <amt262@uakron.edu>
2023-04-03 17:14:25 -0400

    Add product type options

--- react/src/Order.jsx
+++ react/src/Order.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import tshirt from "./assets/blackTShirt.png";
+import longSleeve from "./assets/blackLongSleeve.png";
+import crewneck from "./assets/blackCrewneck.png";
+import hoodie from "./assets/blackHoodie.png";
 import transparentTshirt from "./assets/transparentTshirt.png";
 import transparentLongSleeve from "./assets/transparentLongSleeve.png";
 import transparentCrewneck from "./assets/transparentCrewneck.png";
@@ -8,17 +11,26 @@ import black from "./assets/black.png";
 import red from "./assets/red.png";
 import yellow from "./assets/yellow.png";
 import blue from "./assets/blue.png";
+import designOne from "./assets/designOne.png";
 
 function Order() {
+  const [productType, setProductType] = useState(tshirt);
+
+
   return (
     <div className="Order">
       <div className="row">
         <div className="orderSide">
           <div className="orderFullDesign">
             <img
-            src={tshirt}
+            src={productType}
             alt="Home Team Creativity Logo"
-            className="orderTshirt"
+            className="tshirt"
+            />
+            <img
+              src={designOne}
+              alt="Test"
+              className="orderDesign"
             />
             <br /><br />
             <h3>Product Name</h3>
@@ -34,31 +46,47 @@ function Order() {
         <div className="orderMain">
           <h3>Price calculated after selections</h3>
           <h1>Style: Short Sleeve T-Shirt</h1>
-          <div class="typeOptionRow">
+          <div className="typeOptionRow">
+            <button 
+              onClick={() => setProductType(tshirt)}
+              className="productTypes">
             <img
-                src={transparentTshirt}
-                alt="Home Team Creativity Logo"
-                className="shirtOptions"
+              src={transparentTshirt}
+              alt="Home Team Creativity Logo"
+              className="shirtOptions"
             />
+            </button>
+            <button 
+              onClick={() => setProductType(longSleeve)}
+              className="productTypes">
             <img
-                src={transparentLongSleeve}
-                alt="Home Team Creativity Logo"
-                className="shirtOptions"
+              src={transparentLongSleeve}
+              alt="Home Team Creativity Logo"
+              className="shirtOptions"
             />
+            </button>
+            <button 
+              onClick={() => setProductType(crewneck)}
+              className="productTypes">
             <img
-                src={transparentCrewneck}
-                alt="Home Team Creativity Logo"
-                className="shirtOptions"
+              src={transparentCrewneck}
+              alt="Home Team Creativity Logo"
+              className="shirtOptions"
             />
+            </button>
+            <button 
+              onClick={() => setProductType(hoodie)}
+              className="productTypes">
             <img
-                src={transparentHoodie}
-                alt="Home Team Creativity Logo"
-                className="shirtOptions"
+              src={transparentHoodie}
+              alt="Home Team Creativity Logo"
+              className="shirtOptions"
             />
+            </button>
           </div>
           <br />
           <h1>Color: Black</h1>
-          <div class="typeOptionRow">
+          <div className="typeOptionRow">
             <img
                 src={black}
                 alt="Home Team Creativity Logo"
@@ -82,14 +110,14 @@ function Order() {
           </div>
           <br />
           <h1>Size: Adult Medium</h1>
-          <div class="typeOptionRow">
+          <div className="typeOptionRow">
             <p className="size">YOUTH:</p>
             <p className="size">Small</p>
             <p className="size">Medium</p>
             <p className="size">Large</p>
             <p className="size">X-Large</p>
           </div>
-          <div class="typeOptionRow">
+          <div className="typeOptionRow">
             <p className="size">ADULT:</p>
             <p className="size">Small</p>
             <p className="size">Medium</p>
