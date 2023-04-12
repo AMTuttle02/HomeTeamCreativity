de718265f83ee4316f4a7b8f088474f0dfd4cbe2
Alexander Tuttle <amt262@uakron.edu>
2023-04-11 21:08:40 -0400

    Add backend cart support

--- PHP/src/getCart.php
+++ PHP/src/getCart.php
@@ -9,30 +9,52 @@ session_start();
 
 include 'conn.php';
 
-// Create new user account
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $inputs = json_decode(file_get_contents('php://input'), true);
+// Obtain cart
+$inputs = json_decode(file_get_contents('php://input'), true);
 
-  $orderId = 0;
-  // Insert product to users cart
-  $query = $conn->prepare(
-                        "SELECT *
-                        FROM orders
-                        WHERE user_id = ? AND is_active = 1 AND is_cart = 1");
-  $query->bind_param(
-                    "s",
-                    $_SESSION["userId"]);
-  if (!$query->execute()) {
-    die("Query failed: " . $stmt->error);
-  }
-
-  $result = mysqli_fetch_assoc($query->get_result());
-
-  if (!$result) {
-    die("Result set failed: " . $conn->error);
-  }
-  echo json_encode($result);
+$orderId = 0;
+// Obtain order details
+$query = $conn->prepare(
+                      "SELECT *
+                      FROM orders
+                      WHERE user_id = ? AND is_active = 1 AND is_cart = 1");
+$query->bind_param(
+                  "s",
+                  $_SESSION["userId"]);
+if (!$query->execute()) {
+  die("Query failed: " . $stmt->error);
 }
+
+$result = mysqli_fetch_assoc($query->get_result());
+
+if (!$result) {
+  die("Result set failed: " . $conn->error);
+}
+
+$orderId = $result['order_id'];
+
+$query = $conn->prepare(
+                        "SELECT *
+                        FROM product_orders
+                        INNER JOIN products ON product_orders.product_id = products.product_id
+                        WHERE product_orders.order_id = ?");
+$query->bind_param(
+                  "s",
+                  $orderId);
+if (!$query->execute()) {
+  die("Query failed: " . $stmt->error);
+}
+
+$result = $query->get_result();
+
+$rows = array();
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $rows[] = $row;
+    }
+}
+
+echo json_encode($rows);
 
 mysqli_close($conn);
 
--- react/src/Cart.jsx
+++ react/src/Cart.jsx
@@ -5,6 +5,8 @@ import cart from "./assets/cart.png";
 
 function Cart() {
   const [userId, setUserId] = useState("");
+  const [products, setProducts] = useState([]);
+  const [order, setOrder] = useState([]);
 
   useEffect(() => {
     fetch("/api/session.php")
@@ -14,70 +16,97 @@ function Cart() {
       });
   }, []);
 
-    return (
-      <div className="mycart">
-        {userId ?
-        <div>
-          <br/>
-          <div className="row">
-            <div className="cartSide">
-              <Link to="/products" className="ReturnShopping">
-                Continue Shopping
-              </Link>
-            </div>
-            <div className="cartMain">
-              <div className="row">
-                <div className="myCartSide">
-                  <img src={cart} alt="Cart Image" className="cart"/>
-                </div>
-                <div className="myCartMain">
-                  <h1>My Cart</h1>
-                </div> 
-                <div className="myCartSide">
-                  <img src={cart} alt="Cart Image" className="cart"/>
-                </div>
+  useEffect(() => { 
+    fetch("/api/getOrder.php")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setOrder(data);
+    });
+  }, []);
+
+  useEffect(() => { 
+    fetch("/api/getCart.php")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setProducts(data);
+    });
+  }, []);
+
+  return (
+    <div className="mycart">
+      {userId ?
+      <div>
+        <br/>
+        <div className="row">
+          <div className="cartSide">
+            <Link to="/products" className="ReturnShopping">
+              Continue Shopping
+            </Link>
+          </div>
+          <div className="cartMain">
+            <div className="row">
+              <div className="myCartSide">
+                <img src={cart} alt="Cart Image" className="cart"/>
+              </div>
+              <div className="myCartMain">
+                <h1>My Cart</h1>
+              </div> 
+              <div className="myCartSide">
+                <img src={cart} alt="Cart Image" className="cart"/>
               </div>
             </div>
-            <div className="cartSide">
-              <h1 className="ItemCount"> item(s)</h1>
-            </div>
           </div>
-          <br />
+          <div className="cartSide">
+            <h1 className="ItemCount"> {products.length} item(s)</h1>
+          </div>
+        </div>
+        <br />
 
-          <div className="CartPage" />
-          <div className="row">
-            <div className="productSide">
-              <div className="fullDesign">
-                <img
-                  src={tshirt}
-                  alt="Home Team Creativity Logo"
-                  className="tshirt"
-                />
+        <div className="CartPage" />
+          
+            {products.map((product) => (
+              <div key={product.product_id}>
+                <div className="row">
+                  <div className="productSide">
+                    <div className="fullDesign">
+                      <img
+                        src={tshirt}
+                        alt="Home Team Creativity Logo"
+                        className="tshirt"
+                      />
+                      <img
+                        src={"api/images/" + product.filename}
+                        alt={product.filename}
+                        className="design"
+                      />
+                    </div>
+                  </div>
+                  <div className="productSide">
+                    <br />
+                    <h2> <b> {product.product_name} </b></h2> 
+                    <h2> Style: {product.product_type} </h2>
+                    <h2> Size: {product.size} </h2>
+                    <h2> Color: {product.color} </h2>
+                  </div>
+                  <div className="productSide">
+                    <br />
+                    <h2>$ {product.price} </h2>
+                    <br /><br /><br /><br /><br /><br /><br />
+                    <h2>Qty: {product.product_quantity} </h2>
+                  </div>
+                  <div className="productSide">
+                    <br /><br /><br /><br /><br /><br />
+                    <h2>$ {product.price}</h2>
+                  </div>
+                  <div className="CartPage" />
+                </div>
               </div>
-            </div>
-            <div className="productSide">
-              <br />
-              <h2> <b> Product Name </b></h2> 
-              <h2> Style: </h2>
-              <h2> Size: </h2>
-              <h2> Color: </h2>
-            </div>
-            <div className="productSide">
-              <br />
-              <h2>$</h2>
-              <br /><br /><br /><br /><br /><br /><br />
-              <h2>Qty: </h2>
-            </div>
-            <div className="productSide">
-              <br /><br /><br /><br /><br /><br />
-              <h2>$20.00</h2>
-            </div>
-          </div>
-          <div className="CartPage" />
-
+            ))}
           <br/>
           <div className = "CheckoutButtonPlacement">
-            <h1> Total: </h1>
+            <h1> Total: {order.total_cost}</h1>
             <div className="CartPage" />
             <br/>
             <Link to="/payment" className="CheckoutButton">
@@ -88,20 +117,20 @@ function Cart() {
           <br />
           <br />
         </div>
-        :
-        <div>
-          <center>
-            <br/>
-            <h1>Sorry, you must be logged in to view your cart.</h1>
-            <br />
-            <Link to="/login" className="addToCart">
-              Login Here
-            </Link>
-          </center>
-        </div>
-        }
+      :
+      <div>
+        <center>
+          <br/>
+          <h1>Sorry, you must be logged in to view your cart.</h1>
+          <br />
+          <Link to="/login" className="addToCart">
+            Login Here
+          </Link>
+        </center>
       </div>
-    );
+      }
+    </div>
+  );
 
 }
 export default Cart;
