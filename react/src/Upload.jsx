2e12b066f273da93cedd48c798e11cf37a311da4
Alexander Tuttle <amt262@uakron.edu>
2023-04-25 21:24:07 -0400

    Add available color options to upload page

--- react/src/Upload.jsx
+++ react/src/Upload.jsx
@@ -75,7 +75,111 @@ function Upload() {
               placeholder="Tags"
               onChange={(event) => setTags(event.target.value)}
             />
+            <br/>
+            <center>
+              <h3>Color Options</h3>
+            </center>
+            <br/>
+            <div className="row">
+              <div className="uploadSplit">
+                <label>T-Shirt:</label>
+                <br />
+                <input type="checkbox" id="tBlack" name="tBlack" value="tBlack" />
+                  <label for="black">&nbsp;Black</label>
+                  <br />
+                <input type="checkbox" id="tYellow" name="tYellow" value="tYellow" />
+                  <label for="black">&nbsp;Yellow</label>
+                  <br />
+                <input type="checkbox" id="tPink" name="tPink" value="tPink" />
+                  <label for="black">&nbsp;Pink</label>
+                  <br />
+                <input type="checkbox" id="tGray" name="tGray" value="tGray" />
+                  <label for="black">&nbsp;Gray</label>
+                  <br />
+                <input type="checkbox" id="tMaroon" name="tMaroon" value="tMaroon" />
+                  <label for="black">&nbsp;Maroon</label>
+                  <br />
+                <input type="checkbox" id="tOrange" name="tOrange" value="tOrange" />
+                  <label for="black">&nbsp;Orange</label>
+                  <br />
+                <input type="checkbox" id="tPurple" name="tPurple" value="tPurple" />
+                  <label for="black">&nbsp;Purple</label>
+                  <br />
+                <input type="checkbox" id="tRed" name="tRed" value="tRed" />
+                  <label for="black">&nbsp;Red</label>
+                  <br />
+                <input type="checkbox" id="tRoyal" name="tRoyal" value="tRoyal" />
+                  <label for="black">&nbsp;Royal</label>
+                  <br />
+                <input type="checkbox" id="tGreen" name="tGreen" value="tGreen" />
+                  <label for="black">&nbsp;Green</label>
+                  <br />
+                <input type="checkbox" id="tWhite" name="tWhite" value="tWhite" />
+                  <label for="black">&nbsp;White</label>
+                  <br />
+                <input type="checkbox" id="tNavy" name="tNavy" value="tNavy" />
+                  <label for="black">&nbsp;Navy (Youth Only)</label>
+                  <br />
+              </div>
+              <div className="uploadSplit">
+                <label>Long Sleeve:</label>
+                <br />
+                <input type="checkbox" id="lBlack" name="lBlack" value="lBlack" />
+                  <label for="black">&nbsp;Black</label>
+                  <br />
+                <input type="checkbox" id="lNavy" name="lNavy" value="lNavy" />
+                  <label for="black">&nbsp;Navy</label>
+                  <br />
+                <input type="checkbox" id="lRed" name="lRed" value="lRed" />
+                  <label for="black">&nbsp;Red</label>
+                  <br />
+                <input type="checkbox" id="lRoyal" name="lRoyal" value="lRoyal" />
+                  <label for="black">&nbsp;Royal</label>
+                  <br />
+                <input type="checkbox" id="lGray" name="lGray" value="lGray" />
+                  <label for="black">&nbsp;Gray</label>
+                  <br />
+                <input type="checkbox" id="lWhite" name="lWhite" value="lWhite" />
+                  <label for="black">&nbsp;White</label>
+                  <br />
+              </div>
+              <div className="uploadSplit">
+                <label>Crewneck:</label>
+                <br />
+                <input type="checkbox" id="cBlack" name="cBlack" value="cBlack" />
+                  <label for="black">&nbsp;Black</label>
+                  <br />
+                <input type="checkbox" id="cGray" name="cGray" value="cGray" />
+                  <label for="black">&nbsp;Gray</label>
+                  <br />
+                <input type="checkbox" id="cWhite" name="cWhite" value="cWhite" />
+                  <label for="black">&nbsp;White</label>
+                  <br />
+              </div>
+              <div className="uploadSplit">
+                <label>Hoodie:</label>
+                <br />
+                <input type="checkbox" id="hBlack" name="hBlack" value="hBlack" />
+                  <label for="black">&nbsp;Black</label>
+                  <br />
+                <input type="checkbox" id="hGray" name="hGray" value="hGray" />
+                  <label for="black">&nbsp;Gray</label>
+                  <br />
+                <input type="checkbox" id="hRed" name="hRed" value="hRed" />
+                  <label for="black">&nbsp;Pink</label>
+                  <br />
+                <input type="checkbox" id="hNavy" name="hNavy" value="hNavy" />
+                  <label for="black">&nbsp;Navy</label>
+                  <br />
+                <input type="checkbox" id="hWhite" name="hWhite" value="hWhite" />
+                  <label for="black">&nbsp;White</label>
+                  <br />
+              </div>
+            </div>
+            <br/><br/>
             <input type="file" onChange={handleFileInputChange} />
+            <br/>
+            <br/>
             <button type="submit">Upload</button>
           </form>
         </div>
