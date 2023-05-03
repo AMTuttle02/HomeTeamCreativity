68c7dedf28fcabcb1fb9868c507f07a597e3dea8
Alexander Tuttle <amt262@uakron.edu>
2023-05-03 17:46:50 -0400

    Fix Adult size only for navy

--- react/src/Upload.jsx
+++ react/src/Upload.jsx
@@ -165,7 +165,7 @@ function Upload() {
                   <label>&nbsp;White</label>
                   <br />
                 <input type="checkbox" id="tNavy" name="tNavy" value="Navy" onChange={(event) => handleTshirtColor(event.target.value)}/>
-                  <label>&nbsp;Navy (Youth Only)</label>
+                  <label>&nbsp;Navy (Adult Only)</label>
                   <br />
               </div>
               <div className="uploadSplit">
