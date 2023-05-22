5e06e2f2a3cf722ad3a9a6e2642a27957baa999b
Alexander Tuttle <amt262@uakron.edu>
2023-05-22 19:35:17 -0400

    Show default color on upload

--- react/src/UploadSuccess.jsx
+++ react/src/UploadSuccess.jsx
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import tshirt from "./assets/blackTShirt.png";
+import BlackTshirt from "./assets/blackTShirt.png";
+import GrayTshirt from "./assets/GreyTShirt.png";
+import RedTshirt from "./assets/RedTShirt.png";
+import YellowTshirt from "./assets/YellowTShirt.png";
+import PinkTshirt from "./assets/PinkTShirt.png";
+import GreenTshirt from "./assets/GreenTShirt.png";
+import MaroonTshirt from "./assets/MaroonTShirt.png";
+import OrangeTshirt from "./assets/OrangeTShirt.png";
+import PurpleTshirt from "./assets/PurpleTShirt.png";
+import RoyalTshirt from "./assets/RoyalTShirt.png";
+import WhiteTshirt from "./assets/WhiteTShirt.png";
 
 function Upload() {
   const [admin, setAdmin] = useState("");
@@ -13,6 +23,7 @@ function Upload() {
   }, []);
 
   const [design, setDesign] = useState([]);
+  const [color, setColor] = useState(BlackTshirt);
 
   useEffect(() => {
     fetch("/api/recentUpload.php")
@@ -20,6 +31,22 @@ function Upload() {
       .then((data) => {
         console.log(data);
         setDesign(data);
+        const tShirtMap = {
+          "Black": BlackTshirt,
+          "Gray": GrayTshirt,
+          "Yellow": YellowTshirt,
+          "Pink": PinkTshirt,
+          "Green": GreenTshirt,
+          "Maroon": MaroonTshirt,
+          "Orange": OrangeTshirt,
+          "Purple": PurpleTshirt,
+          "Red": RedTshirt,
+          "Royal": RoyalTshirt,
+          "White": WhiteTshirt
+        }
+        const regex = /\S+/;
+        let firstWord = data[0].tColors.match(regex)[0];
+        setColor(tShirtMap[firstWord]);
     });
   }, []);
 
@@ -35,7 +62,7 @@ function Upload() {
                     <div key={product.filename}>
                         <div className="fullDesign">
                             <img
-                            src={tshirt}
+                            src={color}
                             alt="Home Team Creativity Logo"
                             className="tshirt"
                             />
