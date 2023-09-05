aaf5a425ea2559a3543bade687cdb7c7ff67bcd9
Alexander Tuttle <amt262@uakron.edu>
2023-09-05 16:25:33 -0400

    Revert session information

--- react/src/Order.jsx
+++ react/src/Order.jsx
@@ -192,6 +192,7 @@ function Order() {
   const [size, setSize] = useState({description: "Adult Medium", addedCost: 0});
   const [quantity, setQuantity] = useState(1);
   const [failed, setFailed] = useState(false);
+  const [userId, setUserId] = useState("");
 
   const changeColor = (e) => {
     if (e == gray) {
@@ -568,6 +569,14 @@ function Order() {
       }
     })
   };
+
+  useEffect(() => {
+    fetch("/api/session.php")
+      .then((response) => response.json())
+      .then((data) => {
+        setUserId(data.userId);
+      });
+  }, []);
 
   useEffect(() => {
     if (currentDesign) {
