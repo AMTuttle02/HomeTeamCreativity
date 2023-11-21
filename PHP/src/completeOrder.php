373798164cb9474f9de61145ed17882b9ed4d3be
Alexander Tuttle <amt262@uakron.edu>
2023-11-21 15:17:56 -0500

    Remove file once order is complete

--- PHP/src/completeOrder.php
+++ PHP/src/completeOrder.php
@@ -27,6 +27,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             echo json_encode(0);
         }
 
+        $file_path = USER_UPLOAD_DIR;
+
+        $query = $conn->prepare("SELECT * FROM product_orders 
+                                WHERE order_id = ?");
+        $query->bind_param(
+            "s",
+            $orderId);
+
+        if (!$query->execute()) {
+            die(json_encode('Top Failed'));
+        }
+        $result = $query->get_result();
+
+        if (!$result) {
+            die("Result set failed: " . $conn->error);
+        }
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $file_path = USER_UPLOAD_DIR;
+                if ($row['customerFilename']) {
+                    $file_path .= $row['customerFilename'];
+                    unlink($file_path);
+                }
+            }
+        }
         echo json_encode(1);
     }
 }
