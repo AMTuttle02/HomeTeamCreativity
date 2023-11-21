5a051fcad8bd94960ea6d0fd3dbcfdc5b9cceee9
Alexander Tuttle <amt262@uakron.edu>
2023-11-21 14:30:23 -0500

    Fix delete from cart file path

--- PHP/src/deleteFromCart.php
+++ PHP/src/deleteFromCart.php
@@ -38,7 +38,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
   if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
-    $file_path .= $row['filename'];
+    if ($row['customerFilename']) {
+      $file_path .= $row['customerFilename'];
+    }
+    else {
+      $file_path = "";
+    }
   }
 
   if (file_exists($file_path)) {
@@ -79,6 +84,44 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         else {
             echo json_encode(1);
         }
+      }
+    }
+  }
+  else {
+    $query = $conn->prepare(
+                            "DELETE FROM product_orders
+                            WHERE order_id = ? 
+                            AND product_id = ?
+                            AND color = ? 
+                            AND product_type = ?
+                            AND size = ?");
+    $query->bind_param(
+                      "sssss",
+                      $input['order_id'],
+                      $input['product_id'],
+                      $input['color'],
+                      $input['product_type'],
+                      $input['size']);
+
+    if (!$query->execute()) {
+      echo json_encode(0);
+    }
+    else {
+      $price = $input['price'];
+
+      $query = $conn->prepare(
+                              "UPDATE orders
+                              SET total_cost = total_cost - $price
+                              WHERE order_id = ?");
+      $query->bind_param(
+                        "s",
+                        $input['order_id']);
+
+      if (!$query->execute()) {
+        die("Query failed: " . $stmt->error);
+      }
+      else {
+        echo json_encode(1);
       }
     }
   }
