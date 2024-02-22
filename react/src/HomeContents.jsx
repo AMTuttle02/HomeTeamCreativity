f26e2247bcd146f3173cfce65258afe654343cf7
Alexander Tuttle <amt262@uakron.edu>
2024-02-21 19:23:30 -0500

    Show front and back on homepage

--- react/src/HomeContents.jsx
+++ react/src/HomeContents.jsx
@@ -1,57 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import BlackTshirt from "./assets/blackTShirt.png";
-import BlackLongSleeve from "./assets/blackLongSleeve.png";
-import BlackCrewneck from "./assets/blackCrewneck.png";
-import BlackHoodie from "./assets/blackHoodie.png";
-import GrayTshirt from "./assets/GreyTShirt.png";
-import GrayLongSleeve from "./assets/GreyLongSleeve.png";
-import GrayCrewneck from "./assets/GreyCrewneckSS.png";
-import GrayHoodie from "./assets/GreyHoodie.png";
-import RedTshirt from "./assets/RedTShirt.png";
-import RedLongSleeve from "./assets/RedLongSleeve.png";
-import RedHoodie from "./assets/RedHoodie.png";
-import YellowTshirt from "./assets/YellowTShirt.png";
-import PinkTshirt from "./assets/PinkTShirt.png";
-import GreenTshirt from "./assets/GreenTShirt.png";
-import MaroonTshirt from "./assets/MaroonTShirt.png";
-import OrangeTshirt from "./assets/OrangeTShirt.png";
-import PurpleTshirt from "./assets/PurpleTShirt.png";
-import RoyalTshirt from "./assets/RoyalTShirt.png";
-import RoyalLongSleeve from "./assets/RoyalLongSleeve.png";
-import NavyTshirt from "./assets/NavyTShirt.png";
-import NavyLongSleeve from "./assets/NavyLongSleece.png";
-import NavyHoodie from "./assets/NavyHoodie.png";
-import WhiteTshirt from "./assets/WhiteTShirt.png";
-import WhiteLongSleeve from "./assets/WhiteLongSleeve.png";
-import WhiteCrewneck from "./assets/WhiteCrewneckSS.png";
-import WhiteHoodie from "./assets/WhiteHoodie.png";
-import BackBlackTshirt from "./assets/TshirtBackView/TSBbk.png";
-import BackBlackLongSleeve from "./assets/LongSleeveBackView/LSBbk.png";
-import BackBlackCrewneck from "./assets/CrewBackView/CSBbk.png";
-import BackBlackHoodie from "./assets/HoodieBackView/HBb.png";
-import BackGrayTshirt from "./assets/TshirtBackView/TSBg.png";
-import BackGrayLongSleeve from "./assets/LongSleeveBackView/LSBg.png";
-import BackGrayCrewneck from "./assets/CrewBackView/CSBg.png";
-import BackGrayHoodie from "./assets/HoodieBackView/HBg.png";
-import BackRedTshirt from "./assets/TshirtBackView/TSBr.png";
-import BackRedLongSleeve from "./assets/LongSleeveBackView/LSBR.png";
-import BackRedHoodie from "./assets/HoodieBackView/HBr.png";
-import BackYellowTshirt from "./assets/TshirtBackView/TSBy.png";
-import BackPinkTshirt from "./assets/TshirtBackView/TSBpk.png";
-import BackGreenTshirt from "./assets/TshirtBackView/TSBgn.png";
-import BackMaroonTshirt from "./assets/TshirtBackView/TSBm.png";
-import BackOrangeTshirt from "./assets/TshirtBackView/TSBo.png";
-import BackPurpleTshirt from "./assets/TshirtBackView/TSBp.png";
-import BackRoyalTshirt from "./assets/TshirtBackView/TSBb.png";
-import BackRoyalLongSleeve from "./assets/LongSleeveBackView/LSBb.png";
-import BackNavyTshirt from "./assets/TshirtBackView/TSBn.png";
-import BackNavyLongSleeve from "./assets/LongSleeveBackView/LSBn.png";
-import BackNavyHoodie from "./assets/HoodieBackView/HBn.png";
-import BackWhiteTshirt from "./assets/TshirtBackView/TSBw.png";
-import BackWhiteLongSleeve from "./assets/LongSleeveBackView/LSBw.png";
-import BackWhiteCrewneck from "./assets/CrewBackView/CSBw.png";
-import BackWhiteHoodie from "./assets/HoodieBackView/HBw.png";
+import DisplayProduct from "./DisplayProduct";
 import { Outlet, Link } from "react-router-dom";
 
 function HomeContents() {
@@ -67,125 +16,6 @@ function HomeContents() {
       navigate("/customOrder");
     }
   };
-
-  const currentColor = (product) => {
-    if (product.default_style === "tshirt") {
-      if (product.style_locations === "front") {
-        const tShirtMap = {
-          "Black": BlackTshirt,
-          "Gray": GrayTshirt,
-          "Yellow": YellowTshirt,
-          "Pink": PinkTshirt,
-          "Green": GreenTshirt,
-          "Maroon": MaroonTshirt,
-          "Orange": OrangeTshirt,
-          "Purple": PurpleTshirt,
-          "Red": RedTshirt,
-          "Royal": RoyalTshirt,
-          "White": WhiteTshirt,
-          "Navy": NavyTshirt
-        }
-        const regex = /\S+/;
-        let firstWord = product.tColors.match(regex)[0];
-        return(tShirtMap[firstWord]);
-      }
-      else {
-        const BackTShirtMap = {
-          "Black": BackBlackTshirt,
-          "Gray": BackGrayTshirt,
-          "Yellow": BackYellowTshirt,
-          "Pink": BackPinkTshirt,
-          "Green": BackGreenTshirt,
-          "Maroon": BackMaroonTshirt,
-          "Orange": BackOrangeTshirt,
-          "Purple": BackPurpleTshirt,
-          "Red": BackRedTshirt,
-          "Royal": BackRoyalTshirt,
-          "White": BackWhiteTshirt,
-          "Navy": BackNavyTshirt
-        }
-        const regex = /\S+/;
-        let firstWord = product.tColors.match(regex)[0];
-        return(BackTShirtMap[firstWord]);
-      }
-    }
-    if (product.default_style === "longsleeve") {
-      if (product.style_locations === "front") {
-        const lShirtMap = {
-          "Black": BlackLongSleeve,
-          "Gray": GrayLongSleeve,
-          "Red": RedLongSleeve,
-          "Royal": RoyalLongSleeve,
-          "White": WhiteLongSleeve,
-          "Navy": NavyLongSleeve
-        }
-        const regex = /\S+/;
-        let firstWord = product.lColors.match(regex)[0];
-        return(lShirtMap[firstWord]);
-      }
-      else {
-        const BacklShirtMap = {
-          "Black": BackBlackLongSleeve,
-          "Gray": BackGrayLongSleeve,
-          "Red": BackRedLongSleeve,
-          "Royal": BackRoyalLongSleeve,
-          "White": BackWhiteLongSleeve,
-          "Navy": BackNavyLongSleeve
-        }
-        const regex = /\S+/;
-        let firstWord = product.tColors.match(regex)[0];
-        return(BacklShirtMap[firstWord]);
-      }
-    }
-    if (product.default_style === "crewneck") {
-      if (product.style_locations === "front") {
-        const crewMap = {
-          "Black": BlackCrewneck,
-          "Gray": GrayCrewneck,
-          "White": WhiteCrewneck
-        }
-        const regex = /\S+/;
-        let firstWord = product.cColors.match(regex)[0];
-        return(crewMap[firstWord]);
-      }
-      else {
-        const crewMap = {
-          "Black": BackBlackCrewneck,
-          "Gray": BackGrayCrewneck,
-          "White": BackWhiteCrewneck
-        }
-        const regex = /\S+/;
-        let firstWord = product.cColors.match(regex)[0];
-        return(crewMap[firstWord]);
-      }
-    }
-    if (product.default_style === "hoodie") {
-      if (product.style_locations === "front") {
-        const hoodieMap = {
-          "Black": BlackHoodie,
-          "Gray": GrayHoodie,
-          "Red": RedHoodie,
-          "White": WhiteHoodie,
-          "Navy": NavyHoodie
-        }
-        const regex = /\S+/;
-        let firstWord = product.hColors.match(regex)[0];
-        return(hoodieMap[firstWord]);
-      }
-      else {
-        const hoodieMap = {
-          "Black": BackBlackHoodie,
-          "Gray": BackGrayHoodie,
-          "Red": BackRedHoodie,
-          "White": BackWhiteHoodie,
-          "Navy": BackNavyHoodie
-        }
-        const regex = /\S+/;
-        let firstWord = product.hColors.match(regex)[0];
-        return(hoodieMap[firstWord]);
-      }
-    }
-  }
 
   const getPrice = (price, style) => {
     if (style === "tshirt") {
@@ -234,19 +64,8 @@ function HomeContents() {
             {products.map((product) => (
               <div key={product.filename} className="homeProductsCell">
                 <div className="productDetails">
-                    <button onClick={() => orderProduct(product.product_id)} className="magnify">
-                    <div className="fullDesign">
-                    <img
-                      src={currentColor(product)}
-                      alt="Home Team Creativity Logo"
-                      className="tshirt"
-                    />
-                    <img
-                      src={"api/images/" + product.filename}
-                      alt={product.filename}
-                      className="design"
-                    />
-                  </div>
+                  <button onClick={() => orderProduct(product.product_id)} className="magnify">
+                  <DisplayProduct product={product} />
                   <p>{product.product_name}</p>
                   <p>{"$" + (getPrice(product.price, product.default_style)).toFixed(2)}</p>
                   </button>
