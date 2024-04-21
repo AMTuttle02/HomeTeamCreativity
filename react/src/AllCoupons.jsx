c98a225540e714f15256adfaee4af5eb13973d21
Alexander Tuttle <amt262@uakron.edu>
2024-04-21 14:50:30 -0400

    Display coupons

--- react/src/AllCoupons.jsx
+++ react/src/AllCoupons.jsx
@@ -2,12 +2,67 @@ import React, { useEffect, useState } from "react";
 import Coupons from "./Coupons";
 
 function AllCoupons() {
+  const [coupons, setCoupons] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/getCoupons.php")
+      .then((response) => response.json())
+      .then((data) => {
+        setCoupons(data);
+        console.log(data);
+      })
+  }, []);
+
+  const formatTime = (timeString) => {
+    const date = new Date(timeString);
+    if (isNaN(date.getTime())) {
+      // Check if date is invalid
+      return "No End Date";
+    } else {
+      return date.toLocaleString();
+    }
+  };
+
   return (
     <div className='Dashboard'>
       <br />
       <div className="dashboardContainer">
         <Coupons />
-        <p>All Coupon Code</p>
+        <br />
+        <table className="coupon-table">
+          <thead>
+            <tr>
+              <th>Code</th>
+              <th>Description</th>
+              <th>Amount</th>
+              <th>Type</th>
+              <th>Minimum Required</th>
+              <th>Maximum Allowed</th>
+              <th>Start Time</th>
+              <th>End Time</th>
+              <th>Categories</th>
+              <th>Edit</th>
+            </tr>
+          </thead>
+          <tbody>
+            {coupons.map((coupon, index) => (
+              <tr key={index}>
+                <td>{coupon.code}</td>
+                <td>{coupon.description}</td>
+                <td>{coupon.amount}</td>
+                <td>{coupon.type}</td>
+                <td>{coupon.minimum_required}</td>
+                <td>{coupon.maximum_allowed}</td>
+                <td>{formatTime(coupon.start_time)}</td>
+                <td>{formatTime(coupon.end_time)}</td>
+                <td>{coupon.categories}</td>
+                <td>
+                  <button className="edit-button">Edit</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
