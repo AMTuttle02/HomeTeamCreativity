7f19b1f6b28b4ce42b5f72e6334a020a39439e40
Alexander Tuttle <amt262@uakron.edu>
2024-08-24 20:45:47 -0400

    Finish homepage styling

--- react/src/homepage/HomeContents.jsx
+++ react/src/homepage/HomeContents.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import DisplayProduct from "../products/DisplayProduct";
 import { Outlet, Link, useNavigate } from "react-router-dom";
+import "./homeContents.css";
 
 function HomeContents() {
   const [firstName, setFirstName] = useState("");
