c7fa12370ab20838be09896702784a3b94245655
Alexander Tuttle <amt262@uakron.edu>
2024-08-24 20:51:24 -0400

    Refactor calculator styling

--- react/src/calculator/CostCalculator.jsx
+++ react/src/calculator/CostCalculator.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./calculator.css";
 
 function CostCalculator() {
   const [admin, setAdmin] = useState("");
@@ -26,9 +27,9 @@ function CostCalculator() {
 
   if (admin) {
     return (
-      <div className="UpdatedLogin">
+      <div className="CostCalculator">
         <br />
-        <div className="LoginPage">
+        <div className="CalcPage">
           <div className="container">
             <h1>
               Cost Calculator
