702024792e9098cbbbbe6e116b49b5e6a1d6f0eb
Alexander Tuttle <amt262@uakron.edu>
2024-09-03 22:31:17 -0400

    Refactor cart styling

--- react/src/cart/Cart.jsx
+++ react/src/cart/Cart.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import cart from "../assets/cart.png";
 import DisplayProduct from "../products/DisplayProduct";
+import "./cart.css";
 
 function Cart() {
   const [products, setProducts] = useState([]);
