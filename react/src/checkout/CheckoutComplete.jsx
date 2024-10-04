281c4995d0e50e698a275fd24ca842fd54d3e8c4
Alexander Tuttle <amt262@uakron.edu>
2024-10-03 22:25:43 -0400

    Fix issuesn with merge

--- react/src/checkout/CheckoutComplete.jsx
+++ react/src/checkout/CheckoutComplete.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import DisplayProduct from "./DisplayProduct";
+import DisplayProduct from "../products/DisplayProduct";
 
 function Checkout() {
   const [products, setProducts] = useState([]);
