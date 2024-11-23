5b9f6ceaa793e9750efde0e3a26c3df0da1f4293
Alexander Tuttle <amt262@uakron.edu>
2024-11-23 16:37:48 -0500

    feat-183_Style navbar for mobile

--- react/src/homepage/Navbar.jsx
+++ react/src/homepage/Navbar.jsx
@@ -3,16 +3,28 @@ import { Outlet, Link, useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
 import cart from "../assets/cart.png";
 import profile from "../assets/profile.png";
-import searchGlass from "../assets/Magnifyingglass.png";
 import './homepage.css';
+import searchGlass from "../assets/Magnifyingglass.png";
 
 function Navbar() {
-  const navigate = useNavigate();
-
-  const [searchContents, setSearchContents] = useState("");
   const [totalItems, setTotalItems] = useState(0);
   const [navbarContent, setNavbarContent] = useState([]);
-  const [firstName, setFirstName] = useState("");
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 500);
+  const [isSearchActive, setIsSearchActive] = useState(false);
+  const [searchContents, setSearchContents] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 500);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   useEffect(() => {
     fetch("/api/admin/getNavbar.php", {
@@ -25,7 +37,6 @@ function Navbar() {
     fetch("/api/admin/session.php")
       .then((response) => response.json())
       .then((data) => {
-        setFirstName(data.first_name);
         let oID = 0;
         if (localStorage.getItem("oID")) {
           oID = localStorage.getItem("oID");
@@ -53,6 +64,10 @@ function Navbar() {
       
   }, []);
 
+  const handleSearchClick = () => {
+    setIsSearchActive(!isSearchActive);
+  };
+
   function handleKeyDown(event) {
     if (event.key === "Enter") {
       event.preventDefault();
@@ -76,38 +91,86 @@ function Navbar() {
     }
   }
 
+
   if (navbarContent != []) {
     return (
       <div className="HomePage">
-        <div className="navBar">
-          <Link to="/" className="homeLink">
-              <img src={logo} alt="Home Team Creativity Logo" className="logo"/>
-          </Link>
-          {navbarContent.map((item) => (
-              <Link to={item.link} className="navLink" key={item.id}>
-                {item.name}
-              </Link>
-          ))}
-          <form id="search" className="searchBox">
-            <input type="text" className="search" value={searchContents} onChange={(event) => setSearchContents(event.target.value)} onKeyDown={handleKeyDown} />
-            <img src={searchGlass} alt="Magnifying Glass" className="magnifyingGlass" />
-          </form>
-          <Link to="/login" className="profileLink">
-              <img src={profile} alt="Profile" className="profile" />
-          </Link>
-          <Link to="cart" className="cartLink">
-              <div className="imageSize">
-                <img src={cart} alt="Cart" className="cart" />
-                {totalItems ? 
-                  <div className="circle">
-                    <span className="number">{totalItems}</span>
-                  </div>
-                :
-                  <span />
-                }
+        {isMobile ? 
+          <div className="navBar">
+            <div class="dropdown">
+              <button class="dropbtn">
+              <div class="dots">
+                <span class="dot"></span>
+                <span class="dot"></span>
+                <span class="dot"></span>
               </div>
-          </Link>
-        </div>
+              </button>
+              <div class="dropdown-content">
+                {navbarContent.map((item) => (
+                <Link to={item.link} key={item.id}>
+                  {item.name}
+                </Link>
+                ))}
+              </div>
+            </div>
+            <img src={searchGlass} alt="Magnifying Glass" className="magnifyingGlass" onClick={handleSearchClick}/>
+            {isSearchActive ? <>
+              <form id="search" className="searchBox">
+                <input type="text" className="search" value={searchContents} onChange={(event) => setSearchContents(event.target.value)} onKeyDown={handleKeyDown} />
+              </form>
+              </>
+            : <>
+              <Link to="/" className="homeLink">
+                  <img src={logo} alt="Home Team Creativity Logo" className="logo"/>
+              </Link>
+              <Link to="/login" className="profileLink">
+                  <img src={profile} alt="Profile" className="profile" />
+              </Link>
+              <Link to="cart" className="cartLink">
+                  <div className="imageSize">
+                    <img src={cart} alt="Cart" className="cart" />
+                    {totalItems ? 
+                      <div className="circle">
+                        <span className="number">{totalItems}</span>
+                      </div>
+                    :
+                      <span />
+                    }
+                  </div>
+              </Link>
+            </>}
+          </div>
+        :
+          <div className="navBar">
+            <Link to="/" className="homeLink">
+                <img src={logo} alt="Home Team Creativity Logo" className="logo"/>
+            </Link>
+            {navbarContent.map((item) => (
+                <Link to={item.link} className="navLink" key={item.id}>
+                  {item.name}
+                </Link>
+            ))}
+            <form id="search" className="searchBox">
+              <input type="text" className="search" value={searchContents} onChange={(event) => setSearchContents(event.target.value)} onKeyDown={handleKeyDown} />
+              <img src={searchGlass} alt="Magnifying Glass" className="magnifyingGlass" />
+            </form>
+            <Link to="/login" className="profileLink">
+                <img src={profile} alt="Profile" className="profile" />
+            </Link>
+            <Link to="cart" className="cartLink">
+                <div className="imageSize">
+                  <img src={cart} alt="Cart" className="cart" />
+                  {totalItems ? 
+                    <div className="circle">
+                      <span className="number">{totalItems}</span>
+                    </div>
+                  :
+                    <span />
+                  }
+                </div>
+            </Link>
+          </div>
+        }
         <Outlet/>
         <footer>
           <br/>
