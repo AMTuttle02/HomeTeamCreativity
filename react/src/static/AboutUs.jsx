9c0a1f2d014f9364a6f7048bc0d93421e417c2c6
Alexander Tuttle <amt262@uakron.edu>
2025-02-07 14:18:49 -0500

    Update about us styling

--- react/src/static/AboutUs.jsx
+++ react/src/static/AboutUs.jsx
@@ -9,37 +9,42 @@ function AboutUs() {
     <div className="AboutUs">
       <br />
       <h1>About Us</h1>
-      <div className="row">
-        <div className="aboutUsMobileSplit70">
-          <div className="AboutParagraph">
-            <h3>
-              I started this company in 2020 with all the free time I had, as I am sure we all did.
-              I grew a passion for creating custom designs and high-quality apparel for everyone.
-              In addition to creating custom apparel, I am a full-time college student majoring in
-              Graphic Design and minoring in Entrepreneurship and Marketing. In my rare free time, I enjoy spending
-              time with my family and friends, listening to music, and crafting.
-            </h3>
+      <div className="topAlignRow">
+        <div className="mobileSplit70">
+          <div className="fullContainer">
+            <div className="AboutParagraph">
+              <h3>
+                I started this company in 2020 with all the free time I had, as I am sure we all did.
+                I grew a passion for creating custom designs and high-quality apparel for everyone.
+                In addition to creating custom apparel, I am a full-time college student majoring in
+                Graphic Design and minoring in Entrepreneurship and Marketing. In my rare free time, I enjoy spending
+                time with my family and friends, listening to music, and crafting.
+                <br />
+                <a href="https://maggietuttle.myportfolio.com" target="_blank" className="portfolioLink">Link To My Portfolio</a>
+              </h3>
+            </div>
           </div>
         </div>
-        <div className="aboutUsMobileSplit30">
+        <div className="mobileSplit30">
           <img src={maggie} alt="Maggie Tuttle: CEO" className="aboutImg"/>
         </div>
       </div>
+      <h1>Our Mission</h1>
       <div className="row">
-        <div className="aboutUsMobileSplit70">
-          <div className="AboutParagraph">
-            <h1>Our Mission</h1>
-            <h3>
-              At HomeTeam Creativity, our mission is to provide affordable, creative, and quality custom apparel
-               that makes young and middle-aged adults happy and comfortable with their outfits.
-            </h3>
+        <div className="mobileSplit70">
+          <div className="fullContainer">
+            <div className="AboutParagraph">
+              <h3>
+                At HomeTeam Creativity, our mission is to provide affordable, creative, and quality custom apparel
+                that makes young and middle-aged adults happy and comfortable with their outfits.
+              </h3>
+            </div>
           </div>
         </div>
-        <div className="aboutUsMobileSplit30">
+        <div className="mobileSplit30">
           <img src={info} alt="About Us Contact Info" className="aboutImg"/>
         </div>
       </div>
-      <a href="https://maggietuttle.myportfolio.com" target="_blank" className="portfolioLink">Link To My Portfolio</a>
     </div>
   );
 }
