770d0158415ce4bfcf12653721e33be35f104337
Alexander Tuttle <amt262@uakron.edu>
2025-07-24 20:04:22 -0400

    Update footer sizing

--- react/src/homepage/Navbar.jsx
+++ react/src/homepage/Navbar.jsx
@@ -232,7 +232,7 @@ function Navbar() {
         <footer>
           {/* Footer Message */}
           <br/>
-          <p>{footer}</p>
+          <h1 className="noBold">{footer}</h1>
           {/* Footer Contents */}
           <div className="doubleRow">
             {footerLinks.map((item) => (
@@ -254,11 +254,11 @@ function Navbar() {
 
             {navbarContent.map((item) => (
               <Link to={item.link} onClick={() => window.scrollTo(0,0)} className="footerHeaderLink" key={item.id}>
-                {item.name}
+                <h1 className="noBold">{item.name}</h1>
               </Link>
             ))}
             <Link to={"/login"} onClick={() => window.scrollTo(0,0)} className="footerHeaderLink" key={"Login"}>
-              Login
+              <h1 className="noBold">Login</h1>
             </Link>
           </div>
         </footer>
